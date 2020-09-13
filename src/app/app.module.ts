ad697ecd852a24d8c366e48b68dfd9b5b389c80b
Anderson Schmidt <schmidtanderson@hotmail.com>
2020-09-13 15:52:42 -0300

    Creating establishment service and fetching establishments list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,16 +8,9 @@ import { HeaderComponent } from './header/header.component';
 import { EstablishmentsComponent } from './establishments/establishments.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    EstablishmentsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, EstablishmentsComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/establishments/establishments.component.ts
+++ src/app/establishments/establishments.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { EstablishmentService } from './establishment.service';
 
 @Component({
   selector: 'app-establishments',
   templateUrl: './establishments.component.html',
-  styleUrls: ['./establishments.component.css']
+  styleUrls: ['./establishments.component.css'],
 })
 export class EstablishmentsComponent implements OnInit {
+  establishments = [];
 
-  constructor() { }
+  constructor(private establishmentService: EstablishmentService) {}
 
   ngOnInit(): void {
+    this.establishmentService
+      .getEstablishments()
+      .subscribe((establishments) => {
+        this.establishments = establishments;
+        console.log(establishments);
+      });
   }
-
 }
