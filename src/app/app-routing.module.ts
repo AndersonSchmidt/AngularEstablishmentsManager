f978ac7642ca914c9cd7797fdd344b586770e3e5
Anderson Schmidt <schmidtanderson@hotmail.com>
2020-09-13 14:19:29 -0300

    Creating and adding establishments component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EstablishmentsComponent } from './establishments/establishments.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/establishments', pathMatch: 'full' },
+  { path: 'establishments', component: EstablishmentsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { EstablishmentsComponent } from './establishments/establishments.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    EstablishmentsComponent
   ],
   imports: [
     BrowserModule,
